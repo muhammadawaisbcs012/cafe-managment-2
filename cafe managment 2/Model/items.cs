04e2dc0bf302a149426850850d0019574559b64f
Awais <ak3911271@gmail.com>
2022-10-26 09:25:36 +0500

    Add project files.

---
+++ cafe managment 2/Model/items.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace cafe_managment_2.Model { }
+public class Items
+{
+    [JsonPropertyName("id")]
+    public int items_id { get; set; }
+    [JsonPropertyName("name")]
+    public string items_name { get; set; }
+    [JsonPropertyName("price")]
+    public int items_price { get; set; }
+    public override string ToString()
+    //To store data in files
+    {
+        return JsonSerializer.Serialize(this);
+    }
---
+++ cafe managment 2/Services/Jsoncafe.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+namespace cafe_managment_2.Services
+public class Jsoncafe
+{
+     {   public Jsoncafe.Jsoncafe(IWebHostEnvironment webHostEnvironment);
+
+    IWebHostEnvironment= webHostEnvironment
+
+
+}
+public IWebHostEnvironment.WebHostEnvironment(get;)
+
+public string JsonFilePath
+    public IEnumerable<Items>.getItemsdata
+    { using(var Json_file=File.Opentext(JsonFilepath))
+   JsonSerializer.Deserialize<Items[]>(Json_file.ReadtoEnd())
+}
+
+{
+    get;
+return Path.Combine(WebHostEnvironment.webRootpath, "data,"Items.json ");
+
+}
+}
